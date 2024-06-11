edde4253fcef7eb1fbdee52fb95ec4916e5ce098
Oleksandr Zabolotnyi <azabolotnyi@landmarkglobal.com>
2024-06-11 15:47:59 +0100

    Minimal test case for process isolation not passing auto_prepend_file into the runner

---
+++ tests/SimpleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase {
+	public function test_it_sees_class_C_from_auto_prepend() {
+		$this->assertNotNull(new Prepended());
+	}
+}
